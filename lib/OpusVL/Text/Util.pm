c79be6f587bbe4f2c751b5f27c4231e317808d05
Colin Newell <colin.newell@gmail.com>
2013-10-30 10:46:51 +0000

    Added new utility methods

--- lib/OpusVL/Text/Util.pm
+++ lib/OpusVL/Text/Util.pm
@@ -5,8 +5,10 @@ use strict;
 use warnings;
 
 our @ISA = qw/Exporter/;
-our @EXPORT_OK = qw/truncate_text wrap_text/;
+our @EXPORT_OK = qw/truncate_text wrap_text string_to_id missing_array_items not_blank split_words line_split/;
 
+use Array::Utils qw/intersect array_minus/;
+use Scalar::Util qw/looks_like_number/;
 
 =head1 NAME
 
@@ -89,6 +91,95 @@ sub wrap_text
     my @lines = $string =~ /\G(.{0,$length}\w\b|.*\w\b)\s*/g;
     return join $separator, @lines;
 }
+
+=head2 string_to_id
+
+Makes a string safe to use as an HTML id.  Converts all non safe characters to _.
+
+    string_to_id('thuds-!this') # 'thuds__this'
+
+=cut
+
+sub string_to_id
+{
+    my $text = shift;
+    my $r = $text =~ s/\s+/_/gr;
+    $r =~ s/[^\w_]//g;
+    return lc $r;
+}
+
+=head2 line_split
+
+Splits a string on line breaks.  Accounts for all 3 types of line break, DOS, MAC and Unix.
+
+    line_split("a\nb\r\nc") # qw/a b c/
+
+=cut
+
+sub line_split
+{
+    my $text = shift;
+    my @lines = split /\r\n|\r|\n/, $text;
+    return @lines
+}
+
+=head2 missing_array_items
+
+Returns the list of items missing.
+
+    $mandatory = [qw/a b c/];
+    $cols = [qw/a b d e f/];
+    missing_array_items($mandatory, $cols); # ['c']
+
+=cut
+
+sub missing_array_items
+{
+    my ($mandatory_fields, $actual_fields) = @_;
+
+    my @mand_found = intersect(@$mandatory_fields, @$actual_fields);
+    unless(scalar @mand_found == scalar @$mandatory_fields)
+    {
+        my @missing = array_minus(@$mandatory_fields, @mand_found);
+        return \@missing;
+    }
+    return;
+}
+
+=head2 not_blank
+
+Returns true if the string provided is not blank.
+
+    not_blank('0') # 0
+    not_blank('')  # 1
+
+=cut
+
+sub not_blank
+{
+    my $value = shift;
+    return 1 if $value;
+    if(looks_like_number($value))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+=head2 split_words
+
+Splits a list of words in a string.  Looks for commas to split the list and strips whitespace.
+
+    split_words('veh1,veh2,veh3') #  qw/veh1 veh2 veh3/
+
+=cut
+
+sub split_words
+{
+    my $value = shift;
+    return split /[\s,]+/, $value;
+}
+
 
 =head1 AUTHOR
 
